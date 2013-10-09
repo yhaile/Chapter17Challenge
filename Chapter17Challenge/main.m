f56c7b5b36c3f4445b2d5e55d13bb2bae0e68b4e
Yonas B Haile <yhaile@md3612-38173-10.matc.madison.login>
2013-10-09 12:50:03 -0500

    Initial Commit

---
+++ Chapter17Challenge/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Chapter17Challenge
+//
+//  Created by Yonas B Haile on 10/9/13.
+//  Copyright (c) 2013 Yonas B Haile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
